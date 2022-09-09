eb77fc7c44943e8ccccd542dd40970fc63eb0308
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2022-09-09 17:07:13 +0200

    removed [,],{,} from module string

--- app/backend/app.py
+++ app/backend/app.py
@@ -5,31 +5,24 @@ import json
 ulr = "http://localhost:8000/api/courses/"
 
 response = requests.get(ulr)
-#print(response.text)
-module_string = response.text
-#print(module_string)
-#list_string = response.text.split(",")
-#module_list = module_string.split("}")
-#list_string = str(module_list)
+
+
 
 def main():
     #import markdown
     #output = markdown.markdown(module_string)
     from mdutils.mdutils import MdUtils
     from mdutils import Html
-
     
-    #f = open('module_book.md','w')
-    #f.write(output)
-    #f.close()
     print("Daten von API")
     mdFile = MdUtils(file_name='Modulhandbuch', title='Modulhandbuch')
     mdFile.new_paragraph("---")
-    splitted_string = module_string.split(',')
-    for string in splitted_string:
-        string.replace("[","'")
-        mdFile.new_paragraph(string)
+    module_string = response.text
+    module_string = module_string.replace('[','').replace(']','').replace('{','').replace('}','').replace('"','')
+    print(module_string)
+    mdFile.new_paragraph(module_string)
 
+    
     mdFile.create_md_file()
     print("Eingaben wurden verwendet")
   
