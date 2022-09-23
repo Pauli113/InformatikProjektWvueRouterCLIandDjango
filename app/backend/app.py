3da0ac6560d9d4fb1ecc8c19f46c76aab136e37b
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2022-09-23 14:11:54 +0200

    added textfields with linebreak

--- app/backend/app.py
+++ app/backend/app.py
@@ -10,7 +10,7 @@ def main():
     
     print("Daten von API")
     module_string = response.text
-    module_string = module_string.replace('[','').replace(']','').replace('{','').replace('}','').replace('"','').replace('\\','').replace('further_information:','').replace('po_list:','## po:').replace(':',': ').replace('linebreak: ','').replace(',## ','\n## ').replace(',---','\n---').replace('---,','---\n')
+    module_string = module_string.replace('[','').replace(']','').replace('{','').replace('}','').replace('"','').replace('\\','').replace('further_information:','').replace('po_list:','## po:').replace(':',': ').replace('linebreak: ','').replace(',## ','\n## ').replace(',---','\n---').replace('---,','---\n').replace('n*','\n    ')
     ##
     print(module_string)
     choice = input("Sollen die Kurse hinzugefügt werden? J/N")
