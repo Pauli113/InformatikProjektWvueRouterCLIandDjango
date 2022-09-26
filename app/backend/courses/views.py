2cef664c0e596d083feb5f803ebbeb7db3f759c1
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2022-09-26 22:45:09 +0200

    order by semester working

--- app/backend/courses/views.py
+++ app/backend/courses/views.py
@@ -6,5 +6,5 @@ from .models import Course
 from .serializers import CourseSerializer
 
 class CoursesViewSet(ModelViewSet):
-    queryset = Course.objects.all()
+    queryset = Course.objects.all().order_by('recommended_semester')
     serializer_class = CourseSerializer
