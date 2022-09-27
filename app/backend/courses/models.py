c710b28c91b73d634546bb8b1aef5abbd43ff772
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2022-09-27 14:42:46 +0200

    order by abbrev

--- app/backend/courses/models.py
+++ app/backend/courses/models.py
@@ -29,3 +29,6 @@ class Course(models.Model):
     po_list = models.JSONField(default='')
     further_information = models.CharField(max_length=1000,default='')
     linebreak = models.CharField(max_length=140,default='')
+
+    class Meta:
+        ordering = ('recommended_semester','module_title')
--- app/backend/courses/views.py
+++ app/backend/courses/views.py
@@ -6,5 +6,5 @@ from .models import Course
 from .serializers import CourseSerializer
 
 class CoursesViewSet(ModelViewSet):
-    queryset = Course.objects.all().order_by('recommended_semester')
+    queryset = Course.objects.all().order_by('recommended_semester','module_abbrev')
     serializer_class = CourseSerializer
